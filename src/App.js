7a415c53e8df391df3a433c3be522829764e1113
Roeck <rons9n@gmail.com>
2020-11-09 19:16:59 -0500

    initial useEffect and alan scripts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React,{useEffect} from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+const alanKey="348d9f5311b75a63cf7904e9f3392aa12e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App=() => {
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command}) => {
+                if(command==='testCommand') {
+                    alert("This works");
+                }
+            }
+        })
+    },[]);
+
     return (
         <div>
             <h1>App</h1>
