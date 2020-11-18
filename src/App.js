cf09b2bc7a22e119ab0457596c2bf11602a987c8
Roeck <rons9n@gmail.com>
2020-11-18 17:56:06 -0500

    initial card components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React,{useEffect} from 'react';
+import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+
+import NewsCards from './components/NewsCards/NewsCards';
 
 const alanKey="348d9f5311b75a63cf7904e9f3392aa12e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App=() => {
+    const [newsArticles,setNewsArticles]=useState([]);
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({command}) => {
-                if(command==='testCommand') {
-                    alert("This works");
+            onCommand: ({command,articles}) => {
+                if(command==='newHeadLines') {
+                    setNewsArticles(articles);
                 }
             }
         })
@@ -18,6 +21,7 @@ const App=() => {
     return (
         <div>
             <h1>App</h1>
+            <NewsCards articles={newsArticles} />
         </div>
     );
 }
---
+++ src/components/NewsCards/NewsCards.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewsCard from '../NewsCard/NewsCard';
+
+const NewsCards=({articles}) => {
+    return (
+        <div>
+            {articles.map((article,i) => (
+                <NewsCards />
+            ))}
+        </div>
+    )
+}
+
+export default NewsCards;
