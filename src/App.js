274e1ee529c87ad2dd9c41ea148b71e64a9b24c6
Roeck <rons9n@gmail.com>
2020-11-19 18:17:55 -0500

    added logo and root route styles

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React,{useState,useEffect} from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
+import useStyles from './styles';
 
-const alanKey =
+const alanKey=
 	'348d9f5311b75a63cf7904e9f3392aa12e956eca572e1d8b807a3e2338fdd0dc/stage';
 
-const App = () => {
-	const [newsArticles, setNewsArticles] = useState([]);
+const App=() => {
+	const [newsArticles,setNewsArticles]=useState([]);
+	const classes=useStyles();
+
 	useEffect(() => {
 		alanBtn({
 			key: alanKey,
-			onCommand: ({ command, articles }) => {
-				if (command === 'newHeadLines') {
+			onCommand: ({command,articles}) => {
+				if(command==='newHeadLines') {
 					setNewsArticles(articles);
 				}
 			}
 		});
-	}, []);
+	},[]);
 
 	return (
 		<div>
-			<h1>App</h1>
+			<div className={classes.logoContainer}>
+				<img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="logo" />
+			</div>
 			<NewsCards articles={newsArticles} />
 		</div>
 	);
