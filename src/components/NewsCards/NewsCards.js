c78ee01f51d0d7a883e4f742939489c0df7412bd
Roeck <rons9n@gmail.com>
2020-11-19 17:47:17 -0500

    added more styling

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Grid,Grow,Typography} from '@material-ui/core';
 
-// import NewsCard from './NewsCard/NewsCard';
+import NewsCard from '../NewsCard/NewsCard';
 import useStyles from './styles.js';
 
 const infoCards=[
@@ -31,6 +31,18 @@ const NewsCards=({articles,activeArticle}) => {
 			</Grow>
 		);
 	}
+
+	return (
+		<Grow in>
+			<Grid className={classes.container} container alignItems="stretch" spacing={3}>
+				{articles.map((article,i) => (
+					<Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+						<NewsCard activeArticle={activeArticle} i={i} article={article} />
+					</Grid>
+				))}
+			</Grid>
+		</Grow>
+	);
 };
 
 export default NewsCards;
