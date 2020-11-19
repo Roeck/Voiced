b4e0ef6ec964d5b378734b3eb653fcd2004f81ce
Roeck <rons9n@gmail.com>
2020-11-18 19:07:01 -0500

    styles and images displaying

--- src/App.js
+++ src/App.js
@@ -1,29 +1,30 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import NewsCards from './components/NewsCards/NewsCards';
 
-const alanKey="348d9f5311b75a63cf7904e9f3392aa12e956eca572e1d8b807a3e2338fdd0dc/stage";
+const alanKey =
+	'348d9f5311b75a63cf7904e9f3392aa12e956eca572e1d8b807a3e2338fdd0dc/stage';
 
-const App=() => {
-    const [newsArticles,setNewsArticles]=useState([]);
-    useEffect(() => {
-        alanBtn({
-            key: alanKey,
-            onCommand: ({command,articles}) => {
-                if(command==='newHeadLines') {
-                    setNewsArticles(articles);
-                }
-            }
-        })
-    },[]);
+const App = () => {
+	const [newsArticles, setNewsArticles] = useState([]);
+	useEffect(() => {
+		alanBtn({
+			key: alanKey,
+			onCommand: ({ command, articles }) => {
+				if (command === 'newHeadLines') {
+					setNewsArticles(articles);
+				}
+			}
+		});
+	}, []);
 
-    return (
-        <div>
-            <h1>App</h1>
-            <NewsCards articles={newsArticles} />
-        </div>
-    );
-}
+	return (
+		<div>
+			<h1>App</h1>
+			<NewsCards articles={newsArticles} />
+		</div>
+	);
+};
 
 export default App;
--- src/components/NewsCards/styles.js
+++ src/components/NewsCards/styles.js
@@ -1,9 +1,9 @@
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles({
-    container: {
-        padding: '0 5%',
-        width: '100%',
-        margin: 0,
-    }
+  container: {
+    padding: '0 5%',
+    width: '100%',
+    margin: 0
+  }
 });
